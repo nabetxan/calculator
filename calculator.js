96c2919767d880756a672557f3eb38987eb83889
nabetxan <119922401+nabetxan@users.noreply.github.com>
2023-03-10 23:09:29 +0900

    feat(wip): putting functions together

--- calculator.js
+++ calculator.js
@@ -1,64 +1,290 @@
 //functions for all of the basic math operators
 
 // add
-
 const add = function (a, b) {
   return a + b;
 };
 
-console.log(add(19, 1));
-
 // subtract
-
 const subtract = function (a, b) {
   return a - b;
 };
-console.log(subtract(25, 5));
 
 // multiply
-
 const multiply = function (a, b) {
   return a * b;
 };
-console.log(multiply(4, 5));
 
 // divide
-
 const divide = function (a, b) {
   return a / b;
 };
-console.log(divide(100, 5));
+
+const operatorObj = {
+  add: {
+    function: add,
+    symbol: "+",
+  },
+  subtract: {
+    function: subtract,
+    symbol: "-",
+  },
+  multiply: {
+    function: multiply,
+    symbol: "×",
+  },
+  divide: {
+    function: divide,
+    symbol: "÷",
+  },
+  equal: {
+    symbol: "=",
+  },
+};
 
 // Create a new function operate that takes an operator and 2 numbers
 // and then calls one of the above functions on the numbers.
-
 const operate = function (operator, a, b) {
-  return operator(a, b);
+  return operatorObj[operator].function(Number(a), Number(b));
 };
 
-console.log(operate(add, 2, 3));
+// get the HTML elements
 
-// Create the functions that populate the display when you click the number buttons.
-//You should be storing the ‘display value’ in a variable somewhere for use in the next step.
+//chat-kun
+// const buttons = document.querySelectorAll(".button");
+// const handleClick = function (event) {
+//   return event.target.id;
+// };
+// buttons.forEach(function (button) {
+//   button.addEventListener("click", handleClick);
+// });
+const btnOne = document.getElementById("btnOne");
+const btnTwo = document.getElementById("btnTwo");
+const btnThree = document.getElementById("btnThree");
+const btnFour = document.getElementById("btnFour");
+const btnFive = document.getElementById("btnFive");
+const btnSix = document.getElementById("btnSix");
+const btnSeven = document.getElementById("btnSeven");
+const btnEight = document.getElementById("btnEight");
+const btnNine = document.getElementById("btnNine");
+const btnZero = document.getElementById("btnZero");
+const btnDot = document.getElementById("btnDot");
+const btnClear = document.getElementById("btnClear");
+const btnDivide = document.getElementById("btnDivide");
+const btnMultiply = document.getElementById("btnMultiply");
+const btnSubtract = document.getElementById("btnSubtract");
+const btnAdd = document.getElementById("btnAdd");
+const btnEqual = document.getElementById("btnEqual");
+const currentDisplay = document.querySelector("#currentDisplay");
+const answerDisplay = document.querySelector("#currentAnswer");
 
-// Make the calculator work! You’ll need to store the first number
-// that is input into the calculator when a user presses an operator,
-// and also save which operation has been chosen and then operate() on them
-// when the user presses the “=” key.
-// You should already have the code that can populate the display,
-// so once operate() has been called, update the display with the ‘solution’ to the operation.
-// This is the hardest part of the project. You need to figure out how to store all the values
-//and call the operate function with them. Don’t feel bad if it takes you a while to figure out the logic.
+let num = "";
+let a = "";
+let b = "";
+let isOperatorClicked = false;
+let operator = "";
+let symbol = "";
+let currentDisplayValue = "";
+
+//button
+// chat-kun
+// btnOne.addEventListener("click", handleClick("1"))
+// btnTwo.addEventListener("click", handleClick("2"))
+// When you are adding an event listener to the button elements, you need to pass
+// the function reference as an argument to the addEventListener method. However,
+// in your code, you are calling the handleClick function with an argument immediately
+// while adding the event listener. So, the handleClick function is actually getting called
+// before the event occurs and its result (undefined) is being passed as the event listener function.
+// To fix this, you can wrap the function reference in an anonymous function, like this:
+
+btnOne.addEventListener("click", function () {
+  inputNumWithClick("1");
+});
+btnTwo.addEventListener("click", function () {
+  inputNumWithClick("2");
+});
+btnThree.addEventListener("click", function () {
+  inputNumWithClick("3");
+});
+btnFour.addEventListener("click", function () {
+  inputNumWithClick("4");
+});
+btnFive.addEventListener("click", function () {
+  inputNumWithClick("5");
+});
+btnSix.addEventListener("click", function () {
+  inputNumWithClick("6");
+});
+btnSeven.addEventListener("click", function () {
+  inputNumWithClick("7");
+});
+btnEight.addEventListener("click", function () {
+  inputNumWithClick("8");
+});
+btnNine.addEventListener("click", function () {
+  inputNumWithClick("9");
+});
+btnZero.addEventListener("click", function () {
+  inputNumWithClick("0");
+});
+btnDot.addEventListener("click", function () {
+  inputNumWithClick(".");
+});
+
+//input numbers and "." . Display control//
+function inputNumWithClick(value) {
+  //can click Dot only once
+  //check if value is "." && it's first time
+  if (value === "." && !num.includes(".")) {
+    num = num + value;
+    currentDisplayValue = num;
+    btnDot.disabled = true;
+    return;
+  }
+
+  num = num + value;
+  if (a === "") {
+    // do nothing
+  } else if (operator !== "") {
+    b = num;
+    answerDisplay.textContent = operate(operator, a, b);
+  }
+  currentDisplayValue = currentDisplay.textContent + value;
+  currentDisplay.textContent = currentDisplayValue;
+}
+
+//input symbol. Display control//
+function inputSymbol(value) {
+  //can click only once
+  if (currentDisplay.textContent.slice(-3) === " " + value + " ") {
+    return;
+  }
+  currentDisplayValue = currentDisplay.textContent + " " + value + " ";
+  a = num;
+  b = "";
+  currentDisplay.textContent = currentDisplayValue;
+  btnDot.disabled = false;
+}
+
+//operator
+function inputOperator(value) {
+  //do nothing if no numbers are input
+  if (a === "" && b === "") {
+    return;
+  }
+
+  if (a === num) {
+    operator = value;
+    symbol = operatorObj[operator].symbol;
+    inputSymbol(symbol);
+  } else if (a !== "" && b === "" && value !== "equal") {
+    operator = value;
+    num = "";
+    symbol = operatorObj[operator].symbol;
+    inputSymbol(symbol);
+  } else if (a !== "" && b !== "" && value !== "equal") {
+    operator = value;
+    num = "";
+    symbol = operatorObj[operator].symbol;
+    inputSymbol(symbol);
+    a = b;
+    b = "";
+    // answerDisplay.textContent = operate(operator, a, b);
+    // a = answerDisplay.textContent;
+    // b = "";
+  } else if (value === "equal") {
+    b = num;
+    num = "";
+    symbol = operatorObj[value].symbol;
+    inputSymbol(symbol);
+    answerDisplay.textContent = operate(operator, a, b);
+    a = b;
+    b = "";
+  }
+}
+
+btnDivide.addEventListener("click", function () {
+  inputOperator("divide");
+  isOperatorClicked = false;
+});
+
+btnMultiply.addEventListener("click", function () {
+  inputOperator("multiply");
+  isOperatorClicked = false;
+});
+
+btnSubtract.addEventListener("click", function () {
+  inputOperator("subtract");
+  isOperatorClicked = false;
+});
+
+btnAdd.addEventListener("click", function () {
+  inputOperator("add");
+  isOperatorClicked = false;
+});
+
+btnClear.addEventListener("click", function () {
+  // reset with alert
+  num = "";
+  a = "";
+  b = "";
+  isOperatorClicked = false;
+  operator = "";
+  symbol = "";
+  currentDisplayValue = "";
+  currentDisplay.textContent = "";
+  answerDisplay.textContent = 0;
+});
+
+btnEqual.addEventListener("click", function () {
+  inputOperator("equal");
+});
 
 // Gotchas: watch out for and fix these bugs if they show up in your code:
-// Users should be able to string together several operations and get the right answer, with each pair of numbers being evaluated at a time. For example, 12 + 7 - 5 * 3 = should yield 42. An example of the behavior we’re looking for would be this student solution.
-// Your calculator should not evaluate more than a single pair of numbers at a time. Example: you press a number button (12), followed by an operator button (+), a second number button (7), and finally a second operator button (-). Your calculator should then do the following: first, evaluate the first pair of numbers (12 + 7), second, display the result of that calculation (19), and finally, use that result (19) as the first number in your new calculation, along with the next operator (-).
+// Users should be able to string together several operations and get the right answer,
+//with each pair of numbers being evaluated at a time. For example, 12 + 7 - 5 * 3 = should yield 42.
+//An example of the behavior we’re looking for would be this student solution.
+
 // You should round answers with long decimals so that they don’t overflow the screen.
 // Pressing = before entering all of the numbers or an operator could cause problems!
 // Pressing “clear” should wipe out any existing data.. make sure the user is really starting fresh after pressing “clear”
 // Display a snarky error message if the user tries to divide by 0… and don’t let it crash your calculator!
 
-// Users can get floating point numbers if they do the math required to get one, but they can’t type them in yet. Add a . button and let users input decimals! Make sure you don’t let them type more than one though: 12.3.56.5. It is hard to do math on these numbers. (disable the decimal button if there’s already one in the display)
-// Make it look nice! This is a great project to practice your CSS skills. At least make the operations a different color from the keypad buttons.
+// Users can get floating point numbers if they do the math required to get one, but they can’t type them in yet.
+//Add a . button and let users input decimals! Make sure you don’t let them type more than one though: 12.3.56.5.
+//It is hard to do math on these numbers. (disable the decimal button if there’s already one in the display)
 // Add a “backspace” button, so the user can undo if they click the wrong number.
-// Add keyboard support! You might run into an issue where keys such as (/) might cause you some trouble. Read the MDN documentation for event.preventDefault to help solve this problem.
+// Add keyboard support! You might run into an issue where keys such as (/) might cause you some trouble.
+//Read the MDN documentation for event.preventDefault to help solve this problem.
+
+// Keyboard support: Allow users to use the keyboard to input numbers and operators instead of clicking the buttons. You can achieve this by listening for keydown events and mapping the pressed key to its corresponding button.
+
+// Negative numbers: Add support for negative numbers by allowing the user to input a negative sign (-) before the number.
+
+// Decimal precision: Limit the decimal precision of the result to a certain number of decimal places (e.g., 2) to avoid long decimals.
+
+// Error handling: Add error handling to the code to handle edge cases such as division by zero or inputting invalid characters.
+
+// function inputNumWithClick(value) {
+//   //can click Dot only once
+//   //check if value is "." && it's first time
+//   if (value === "." && isDotClicked === false) {
+//     isDotClicked = true;
+//     num = num + value;
+//     currentDisplayValue = num;
+//   } else if (value === "." && isDotClicked === true) {
+//     // do nothing;
+//     return;
+//   }
+
+//   if (value !== "." && isOperatorClicked === true) {
+//     // num = value; // as "b"
+//     currentDisplayValue = currentDisplay.textContent + " " + value;
+//     isOperatorClicked = false;
+//   } else if (value !== "." && isOperatorClicked === false) {
+//     num = num + value;
+//     currentDisplayValue = value;
+//   }
+
+//   currentDisplay.textContent = currentDisplayValue;
+// }
